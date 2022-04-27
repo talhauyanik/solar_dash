539a876954ee5965d43ed8e5d3c465a8896ea9ed
Talha <talhauyanik3@gmail.com>
2022-04-27 23:24:23 +0300

    config update setup sh

--- setup.sh
+++ setup.sh
@@ -5,5 +5,12 @@ echo "\
 port = $PORT\n\
 enableCORS= false\n\
 headless = true\n\
+[theme]\n\
+base ='light'\n\
+textColor = '#31333F'\n\
+primaryColor = '#ec6e4c'\n\
+backgroundColor = '#ffffff'\n\
+secondaryBackgroundColor = '#F0F2F6'\n\
+font = 'serif'\n\
 \n\
 " > ~/.streamlit/config.toml
