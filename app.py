e4586b8f5552232c182b6a978477f11d7660c919
Talha <talhauyanik3@gmail.com>
2022-04-28 19:20:53 +0300

    fix

--- app.py
+++ app.py
@@ -146,7 +146,7 @@ tahmin_title_1gun = (datetime.date.today() - datetime.timedelta(days=0)).strftim
 tahmin_title_1ay = (datetime.date.today() - datetime.timedelta(days=0)).strftime("%B")
 tahmin_title_2gun = (datetime.date.today() + datetime.timedelta(days=3)).strftime("%d")
 tahmin_title_2ay = (datetime.date.today() + datetime.timedelta(days=3)).strftime("%B")
-st.markdown("<p style='text-align: center; color: #31333f;font-size: 2.25rem;font-weight:bold ;'>4 Günlük Tahmini Üretim ("+tahmin_title_1gun+" "+str(weather.aycevir(tahmin_title_1ay))+" - "+tahmin_title_1gun+" "+str(weather.aycevir(tahmin_title_2ay))+")</p>", unsafe_allow_html=True)
+st.markdown("<p style='text-align: center; color: #31333f;font-size: 2.25rem;font-weight:bold ;'>4 Günlük Tahmini Üretim ("+tahmin_title_1gun+" "+str(weather.aycevir(tahmin_title_1ay))+" - "+tahmin_title_2gun+" "+str(weather.aycevir(tahmin_title_2ay))+")</p>", unsafe_allow_html=True)
 st.markdown("<br>", unsafe_allow_html=True)
 
 colon0, colon1, colon2, colon3, colon4, colon5,  = st.columns([2,1,1,1,1,2])
